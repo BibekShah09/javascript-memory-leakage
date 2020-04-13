43ffd47951d8c0614d2a113f61da1d10c8ab1ca3
Bibek Shah <bibekshah09@gmail.com>
2020-04-13 17:58:49 +0545

    include command line text

---
+++ request-gc.js
@@ -0,0 +1,12 @@
+// Execute the following code with --expose-gc flag: node --expose-gc expose-gc.js
+
+
+try {
+  if (global.gc) {
+    console.log("garbage collection request proceeded");
+    global.gc();
+  }
+} catch (e) {
+  console.log("Failed to proceed");
+  process.exit();
+}
